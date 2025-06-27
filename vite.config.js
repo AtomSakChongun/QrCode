9d3467e5c499ae513dfb43f4ada96122b889f81a
DESKTOP-0JTR50E\User <poochit.sk@gmail.com>
2025-06-27 10:59:15 +0700

    aadd firebase

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/AtomSakChongun/QrCode/',  // แก้ให้ตรง repo path
+  base: './',  // แก้ให้ตรง repo path
   plugins: [react()],
 })
