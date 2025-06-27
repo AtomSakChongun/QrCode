5040a51a1b404a0fcdc4c15bd936b23d7d0a9914
DESKTOP-0JTR50E\User <poochit.sk@gmail.com>
2025-06-27 10:12:10 +0700

    no message

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/AtomSakChongun/QrCode/',  // แก้ให้ตรง repo path
+  base: '/QrCode/',  // แก้ให้ตรง repo path
   plugins: [react()],
 })
